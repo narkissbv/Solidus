f8bb9f6ebdec09683f4f0b0fba02eb6ddedc30ca
Matan Narkiss <dv.narxx@gmail.com>
2020-11-08 21:10:49 +0200

    add chevrons and comments

--- src/components/Solidus.vue
+++ src/components/Solidus.vue
@@ -32,11 +32,36 @@
     <div>
       Events table
     </div>
-    <table>
+    <table class="events-table">
       <tr>
-        <th @click="sortEvents('price')">Price</th>
-        <th @click="sortEvents('status')">Status</th>
-        <th @click="sortEvents('timestamp')">Timestamp</th>
+        <th @click="sortEvents('price')">
+          Price
+          <img src="../assets/chevron-down.svg"
+               :class="{
+                 'reverse' : sorting.price < 0,
+                 'show' : sorting.price
+               }"
+          />
+        </th>
+        <th @click="sortEvents('status')">
+          Status
+          <img src="../assets/chevron-down.svg"
+               :class="{
+                 'reverse' : sorting.status < 0,
+                 'show' : sorting.status
+               }"
+          />
+        </th>
+        <th @click="sortEvents('timestamp')">
+          Timestamp
+          <img src="../assets/chevron-down.svg"
+               :class="{
+                 'reverse' : sorting.timestamp < 0,
+                 'show' : sorting.timestamp
+               }"
+          />
+
+        </th>
       </tr>
       <Event v-for="(event, idx) in filteredEvents" :key="idx"
              :event="{...event, idx}"
@@ -56,9 +81,9 @@ export default {
       eventsDisplay: [],
       filter: 'All',
       sorting: {
-        price: false,
-        status: false,
-        timestamp: false
+        price: 0,
+        status: 0,
+        timestamp: 1
       }
     }
   },
@@ -67,6 +92,11 @@ export default {
   },
   methods: {
     player(step) {
+      // evaluate the commands pressed on the player's action buttons
+      // accepts an integer (positive to more forward, and negative to move backwards)
+      // or the keywords 'start' and 'end' to skip all the way to the beginning / end.
+      // assumes input is valid. No validation mechanism (something that should be
+      // included in unit tests)
       switch (step) {
         case 'start':
           this.currentIdx = 0
@@ -90,31 +120,37 @@ export default {
       }
     },
     selectCurrent(idx) {
+      // set the currently displayed event in the player box to a specific event
+      // clicked on in the events table
       this.currentIdx = idx
     },
     sortEvents (sort) {
+      // reset all other sorting variables (we are only sorting according to one column)
       switch (sort) {
         case 'price':
-          this.sorting.timestamp = false
-          this.sorting.status = false
+          this.sorting.timestamp = 0
+          this.sorting.status = 0
           break
         case 'status':
-          this.sorting.timestamp = false
-          this.sorting.price = false
+          this.sorting.timestamp = 0
+          this.sorting.price = 0
           break
         case 'timestamp':
-          this.sorting.price = false
-          this.sorting.status = false
+          this.sorting.price = 0
+          this.sorting.status = 0
           break
         default:
           break
       }
-      if (this.sorting[sort]) {
+      if (this.sorting[sort] === 0) {
+        this.sorting[sort] = 1
+      }
+      if (this.sorting[sort] > 0) {
         this.filteredEvents.sort( (a, b) => a[sort] > b[sort] ? 1 : -1 )
       } else {
         this.filteredEvents.sort( (a, b) => a[sort] < b[sort] ? 1 : -1 )
       }
-      this.sorting[sort] = !this.sorting[sort]
+      this.sorting[sort] *= -1
     },
   },
   computed: {
@@ -146,20 +182,36 @@ export default {
     this.events.forEach( (event) => {
       event.price = parseInt(event.price)
     })
+    this.sortEvents('timestamp')
   }
 }
 </script>
 
 <style>
-  table {
+  table.events-table {
     cursor: pointer
   }
-  table tr:hover td {
+  table.events-table th {
+    font-size: 24px;
+    border-bottom: 2px solid gray
+  }
+  table.events-table th img {
+    display: none;
+  }
+  table.events-table tr:hover td {
     background: lightgray
+  }
+  table.events-table th img.show {
+    display: inline-block;
+  }
+  table.events-table th img.reverse {
+    transform: rotate(180deg);
+    transition: all 0.3s ease;
   }
 
   .player,
   .filter {
-    margin-bottom: 30px;
+    margin-bottom: 40px;
   }
+
 </style>
